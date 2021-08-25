f39b1e02bb080426090b91dcd774911ef8a64061
Shravan Seth <64133116+ShravanSeth@users.noreply.github.com>
2021-08-25 11:24:19 +0530

    Port Updated

--- app.js
+++ app.js
@@ -158,10 +158,11 @@ app.post("Sign_up", function(req,res){
     })
 })
 
-let port = process.env.PORT;
+const port = process.env.PORT;
 if (port == null || port == "") {
   port = 3000;
 }
+app.set("port",PORT)
 app.listen(port, function() {
   console.log("Server started on port 3000");
 });
